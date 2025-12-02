37ee5e4b62ffbc3b7215de66bc3dc2c225260b25
susalohar-byte <susalohar@gmail.com>
2025-12-02 09:53:59 +0530

    Updated app.js

--- admin/app.js
+++ admin/app.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'https://0ec90b57d6e95fcbda19832f.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJib2x0IiwicmVmIjoiMGVjOTBiNTdkNmU5NWZjYmRhMTk4MzJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4ODE1NzQsImV4cCI6MTc1ODg4MTU3NH0.9I8-U0x86Ak8t2DGaIk0HfvTSLsAyzdnz-Nw00mMkKw';
+const SUPABASE_URL = 'https://gjrofevnjsxvftlgsqvz.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdqcm9mZXZuanN4dmZ0bGdzcXZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzMTk3NDQsImV4cCI6MjA3OTg5NTc0NH0.ipVAH38O7X_j-zZpaTxJ9jYSjOAVvmRpX2QxHVf1KXg';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
