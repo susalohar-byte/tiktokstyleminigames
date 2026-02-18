9b12290fb79f056abfc1bbd46285659a3d28402c
susalohar-byte <susalohar@gmail.com>
2026-02-18 14:08:48 +0530

    Updated app.json

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -64,6 +64,7 @@ export default function ExploreScreen() {
       </View>
 
       <FlatList
+        key="categories-grid"
         data={categories}
         keyExtractor={(item) => item.id}
         numColumns={2}
